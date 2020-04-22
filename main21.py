df751301a01f7fa3a4d13923353ee2cca811e270
Nemolite <g25092011@mail.ru>
2020-04-22 16:25:51 +0300

    ex3

--- main21.py
+++ main21.py
@@ -5,5 +5,15 @@ print(y)
 
 arr = ['sel',23,2.3,(21,34,17)]
 a1,a2,a3,a4 = arr
+print(a1)
+print(a2)
+print(a3)
+print(a4)
 for a in arr:
 	print(a)
+
+str1 = 'Hel'	
+n1,n2,n3 = str1
+print(n1)
+print(n2)
+print(n3)
---
+++ main22.py
@@ -0,0 +1,9 @@
+class Basa:
+	p1 = 20
+	def metod(self,a):
+		self.a = a
+obj1 = Basa()	
+obj1.metod(22)
+print(obj1.a)
+print(obj1.p1)
+		
