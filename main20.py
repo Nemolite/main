7eb6e4a259e6e7b30cd98cbf3209e6f6d3821f95
Nemolite <g25092011@mail.ru>
2020-04-28 10:52:01 +0300

    tk1

--- main20.py
+++ main20.py
@@ -1,11 +1,11 @@
 import vk_api
 
-vk_session = vk_api.VkApi('номер', 'пароль')
+vk_session = vk_api.VkApi('номер', 'прароль')
 vk_session.auth()
 
 vk = vk_session.get_api()
 
-posts = vk.wall.get(count=1000)['items'] 
+posts = vk.wall.get(count=100)['items'] 
 while(posts):
 	for post in posts:
 		print(post['id'])
